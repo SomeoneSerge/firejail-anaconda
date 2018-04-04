f5ab1be2c4b71df33b950ac4ff88cf00670944e7
Serge Kozlukov <newkozlukov@gmail.com>
2018-04-04 11:07:28 +0300

    initial draft

---
+++ .jupyter/jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.notebook_dir = '/home/USERNAME/nb/'
+c.NotebookApp.open_browser = False
+c.NotebookApp.password = 'sha1:'
+c.NotebookApp.password_required = True
+c.NotebookApp.port = 8888
---
+++ README.md
@@ -0,0 +1,2 @@
+Sandboxed environment for DS.
+ATM, includes firejailed anaconda3 and systemd user unit for jupyterlab.
